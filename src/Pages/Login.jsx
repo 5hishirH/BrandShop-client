0b9ef81c8b06e74c8acdbed0a67e81fcac7f5be3
Shishir Halder <s.halder0108@gmail.com>
2023-10-23 07:37:38 +0600

    feat: create basic login form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
     <div>
-        Login
-    </div>
-  )
-}
+      <form action="">
+        {/* email */}
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
 
-export default Login
+        {/* password */}
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <input type="submit" value="Login" className="input w-full max-w-xs cursor-pointer" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
