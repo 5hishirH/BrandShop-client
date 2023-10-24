7858db03089a1dfa26094e95c39120bf320086e0
Shishir Halder <s.halder0108@gmail.com>
2023-10-24 13:07:33 +0600

    feat: add basic form at add product route

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const AddProduct = () => {
   return (
     <div>
-        AddProduct
+      <form>
+        <input
+          name="image"
+          type="text"
+          placeholder="Enter your img link here"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          name="productName"
+          type="text"
+          placeholder="Brand Name"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          name="brandName"
+          type="text"
+          placeholder="Brand name"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          name="prodType"
+          type="text"
+          placeholder="Product Types"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          name="prodInfo"
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          name="rating"
+          type="number"
+          placeholder="Rating"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="submit"
+          value='Add'
+          className="input input-bordered w-full max-w-xs cursor-pointer"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
