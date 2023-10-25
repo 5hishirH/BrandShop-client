3fd7ff50c551ddff95d8812866df3cf23759722d
Shishir Halder <s.halder0108@gmail.com>
2023-10-25 18:41:22 +0600

    feat: add banner styles

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="w-full h-[70vh] bg-[url(https://i.ibb.co/YpSVh0s/fashion-Banner.jpg)] bg-center bg-cover bg-no-repeat flex justify-center items-center">
+      <div className="text-5xl text-center leading-relaxed font-semibold">
+        <span className="text-some_green">Discover Your Style, Embrace Comfort, and Elevate Your Confidence with</span>
+        <br />
+        <span className="text-white">
+          Our Fashionable Bodywear & Apparel Collection!
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import OurBrands from '../Components/Header/OurBrands'
+import React from "react";
+import OurBrands from "../Components/Header/OurBrands";
+import Banner from "../Components/Banner";
 
 const Home = () => {
   return (
     <div>
-        <OurBrands></OurBrands>
+      <Banner></Banner>
+      <OurBrands></OurBrands>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
